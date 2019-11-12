56b6607096c1e93b5d0c1ca19308c555f709f05d
Vyacheslav <webdev.pv@gmail.com>
2019-11-12 15:56:11 +0200

    tasks 1.1 and 1.2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,33 @@
-// Write your code here
+const express = require('express');
+const fs = require('fs');
+const server = express();
+const port = 4000;
+
+server.get(`/movies`, (req, res) => {
+  fs.readFile('data.json', 'utf-8', (err, data) => {
+    if(err) {
+        console.error(404);
+    } else {
+        const sortMovies = JSON.parse(data)
+          .sort((film1, film2) => (
+            Number(film1.year) - Number(film2.year)
+          ));
+        res.end(JSON.stringify(sortMovies, null, 2));
+    }
+  });
+});
+
+server.get('/movies/titles', (req, res) => {
+  fs.readFile('data.json', 'utf-8', (err, data) => {
+    if(err) {
+      console.error(404);
+    } else {
+      const titles = JSON.parse(data)
+      .map(movie => movie.title)
+      .sort();
+      res.end(titles.join('\n'));
+    }
+  })
+});
+
+server.listen(port, () => console.log('listening...'))
