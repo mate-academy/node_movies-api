f82f744a7a20a10cd72e1ee346920ece737a1d55
vladyslav223 <vladyslav223@gmail.com>
2019-11-12 22:26:43 +0200

    firstPartDone

--- src/server.js
+++ src/server.js
@@ -1,28 +1,58 @@
 const express = require('express');
 const app = express();
+const fs = require('fs'); 
 const port = 5000;
-const data = require('../data.json');
+
 
 app.get('/movies', (req,res) => {
-    res.send(data.sort((a,b) => a.year - b.year));
+    fs.readFile('data.json', (err, data) => {
+        let result = JSON.parse(data.sort((a,b) => a.year - b.year));
+        res.json(result);
+    });    
 });
 
-app.get('/movies/titles', (req,res) => {     
-    const findTitle = data.map(film => film.title + '<br>').sort((a, b) => a.localeCompare(b)).join('');
-    res.send(findTitle ? findTitle : 'Something whent wrong');  
+app.get('/movies/titles',async (req,res) => {     
+    fs.readFile('data.json', (err, data) => {
+        let result = JSON.parse(data);
+
+        if (err) {
+            res.sendStatus(500)
+        } else {
+            if (req.query.year) {
+                result = result.filter(film => +film.year == req.query.year).map(film => film.title);
+
+                if(result.length > 1) {
+                    result = result.sort((a, b) => a.localeCompare(b)).join('\n'); 
+
+                    return res.send(result);
+                } else if (result.length === 1) {
+                    return res.send(result.join('\n'));
+                } else {
+                    res.send('Something whent wrong');
+                }                
+            }
+           
+          result = result.map(film => film.title).sort((a, b) => a.localeCompare(b)).join('\n');
+          res.send(result ? result : 'Something whent wrong');  
+        }
+    }); 
 });
 
-app.get('/movies/:id', (req,res) => {     
-    const findFilm = data.find(film => film.id === req.params.id);    
-    res.send(findFilm ? findFilm : 'Something whent wrong');  
+app.get('/movies/:id',async (req,res) => {     
+    fs.readFile('data.json', (err, data) => {
+        if (err) {
+            res.sendStatus(500)
+        } else {
+          const movie = JSON.parse(data).find(film => film.id === req.params.id); 
+
+          if (movie) {
+              res.send(movie)
+          } else {
+              res.status(404).send(`Error! Not found!${req.params.id}`);
+          }
+        }
+    }); 
 });
-
-
-
-
-
-app.use(express.static('src'));
-
 
 app.listen(port);
 console.log('listening...')
