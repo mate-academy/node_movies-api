755ff2cf0264c2331fead2adf5ea4c5a115fb72f
Dmytro <voronok16@gmail.com>
2019-11-12 22:56:50 +0200

    solution task 1-4

--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
-// Write your code here
+const express = require('express');
+// const fs = require('fs');
+const app = express();
+const port = 4000;
+const data = require('../data.json');
+
+app.get('/movies', (req,res) => res.json([...data].sort((a,b) => a.year - b.year)));
+
+app.get('/movies/titles', (req,res) => {
+    if(req.query.year) {
+        const filteringYearsData = [...data].filter(item => item.year === req.query.year).sort((a,b) => a.title.localeCompare(b.title));
+        res.send(filteringYearsData.map(obj => obj.title).join('\n'));
+    };
+    res.send([...data].map(item => item.title).sort((a,b) => a.localeCompare(b)).join('\n'))
+});
+
+app.get('/movies/:id', (req,res) => {
+    const searchingResult = data.find(item => item.id === req.params.id);
+    if (!searchingResult) {
+        res.sendStatus(404);
+    }
+    res.json(searchingResult);
+});
+
+app.listen(port); 
+
+app.use(express.static('src'));
+
+
