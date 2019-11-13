b04f300e3ad5c59c71b91ea2a6d1c06bdb14ce6d
Vyacheslav <webdev.pv@gmail.com>
2019-11-13 09:07:38 +0200

    added task 1.3 and 1.4

--- src/server.js
+++ src/server.js
@@ -1,33 +1,63 @@
 const express = require('express');
 const fs = require('fs');
+
 const server = express();
 const port = 4000;
 
 server.get(`/movies`, (req, res) => {
-  fs.readFile('data.json', 'utf-8', (err, data) => {
+  fs.readFile('data.json', (err, data) => {
     if(err) {
-        console.error(404);
+      res.sendStatus(500);
     } else {
-        const sortMovies = JSON.parse(data)
-          .sort((film1, film2) => (
-            Number(film1.year) - Number(film2.year)
-          ));
-        res.end(JSON.stringify(sortMovies, null, 2));
+      const sortMovies = JSON.parse(data)
+        .sort((film1, film2) => (
+          Number(film1.year) - Number(film2.year)
+        ));
+
+      res.send(sortMovies);
     }
   });
 });
 
 server.get('/movies/titles', (req, res) => {
-  fs.readFile('data.json', 'utf-8', (err, data) => {
+  fs.readFile('data.json', (err, data) => {
     if(err) {
-      console.error(404);
+      res.sendStatus(500);
     } else {
-      const titles = JSON.parse(data)
-      .map(movie => movie.title)
-      .sort();
-      res.end(titles.join('\n'));
+      let films = JSON.parse(data);
+
+      if (req.query.year) {
+        films = films.filter(film => film.year === req.query.year);
+      }
+
+      if (!films.length) {
+        res.status(404).send("Title not found");
+      } else {
+        res.send(films
+          .map(movie => movie.title)
+          .sort()
+          .join('\n')
+        );
+      }
     }
   })
 });
+
+server.get('/movies/:id', (req, res) => {
+  fs.readFile('data.json', (err, data) => {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      const film = JSON.parse(data)
+      .find(movie => movie.id === req.params.id)
+
+      if (film) {
+        res.send(film);
+      } else {
+        res.status(404).send(`can't find film by id = ${req.params.id}`);
+      }
+    }
+  });
+})
 
 server.listen(port, () => console.log('listening...'))
