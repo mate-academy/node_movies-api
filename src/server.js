f4a0f50a009e3c89d5a80b85853b8bb4739362db
Dmytro <voronok16@gmail.com>
2019-11-14 17:40:43 +0200

    Extra task done, corrected by comments

--- src/server.js
+++ src/server.js
@@ -1,29 +1,129 @@
 const express = require('express');
-// const fs = require('fs');
+const fs = require('fs');
+let moviesList = require('../data.json');
+const uuid = require('uuid/v4'); 
+const bodyParser = require('body-parser'); 
+
 const app = express();
 const port = 4000;
-const data = require('../data.json');
 
-app.get('/movies', (req,res) => res.json([...data].sort((a,b) => a.year - b.year)));
+getTitle = (obj) => obj.map(item => item.title);
+
+sortByWord = (obj) => obj.sort((item1,item2) => item1.localeCompare(item2));
+
+objectFiltering = (obj, prop, value, option) => {
+    if(option === '!==') {
+        return obj.filter(item => item[prop] !== value);
+    } else {
+        return obj.filter(item => item[prop] === value);   
+    };
+};
+
+app.get('/movies', (req,res) => res.json([...moviesList].sort((a,b) => a.year - b.year)));
 
 app.get('/movies/titles', (req,res) => {
     if(req.query.year) {
-        const filteringYearsData = [...data].filter(item => item.year === req.query.year).sort((a,b) => a.title.localeCompare(b.title));
-        res.send(filteringYearsData.map(obj => obj.title).join('\n'));
-    };
-    res.send([...data].map(item => item.title).sort((a,b) => a.localeCompare(b)).join('\n'))
+        const filteringYearsData = objectFiltering(moviesList,'year',req.query.year);
+        const title = getTitle(filteringYearsData);
+
+        res.send(sortByWord(title).join('\n'));
+    } else {
+        res.send(sortByWord(getTitle(moviesList)).join('\n'))
+    }
 });
 
 app.get('/movies/:id', (req,res) => {
-    const searchingResult = data.find(item => item.id === req.params.id);
+    const searchingResult = moviesList.find(movie => movie.id === req.params.id);
     if (!searchingResult) {
         res.sendStatus(404);
     }
     res.json(searchingResult);
 });
 
+app.post('/movies', bodyParser.json(),(req,res) => {
+    const { title, year, imdbRating } = req.body;
+    const newFilm = {
+        id: uuid(),
+        title: title,
+        year: year,
+        imdbRating: imdbRating,
+    };
+    moviesList = [...moviesList, newFilm];
+
+    fs.writeFile('data.json', JSON.stringify(moviesList), (err) => {
+        if (err) {
+            res.send(err);
+          return
+        }
+        res.json(moviesList);
+      })
+});
+
+app.put('/movies/:id', bodyParser.json(),(req,res) => {
+    const { id } = req.params;
+    const { title, imdbRating, year } = req.body;
+    let editedFilm = {};
+
+    moviesList = moviesList.map(movie => {
+        if(id === movie.id) {
+            movie = {
+                id,
+                title,
+                imdbRating,
+                year,
+            }
+            editedFilm = movie;
+        }
+        return movie;
+    });
+
+    fs.writeFile('data.json', JSON.stringify(moviesList), (err) => {
+        if (err) {
+            res.send(err);
+          return;
+        }
+        res.json(editedFilm);
+      })
+});
+
+app.patch('/movies/:id', bodyParser.json(),(req,res) => {
+    const { id } = req.params;
+    const { title, imdbRating, year } = req.body;
+    let editedFilm = {};
+
+    moviesList = moviesList.map(movie => {
+        if(id === movie.id) {
+            movie = {
+                id,
+                title: title || movie.title,
+                imdbRating: imdbRating || movie.imdbRating,
+                year: year || movie.year,
+            }
+            editedFilm = movie;
+        }
+        return movie;
+    });
+    
+    fs.writeFile('data.json', JSON.stringify(moviesList), (err) => {
+        if (err) {
+            res.send(err);
+          return;
+        }
+        res.json(editedFilm);
+      })
+    });
+
+app.delete('/movies/:id', bodyParser.json(),(req,res) => {
+
+    moviesList = objectFiltering(moviesList, 'id', req.params.id,'!==');
+
+    fs.writeFile('data.json', JSON.stringify(moviesList), (err) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.sendStatus(204);
+    })
+});
+
 app.listen(port); 
-
-app.use(express.static('src'));
-
-
