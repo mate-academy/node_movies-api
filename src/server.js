e564e3c615a2548c35c62d0a3d347ede9f5e119c
igaryok <igor.sobko@gmail.com>
2019-11-19 10:04:39 +0200

    create root page

--- src/server.js
+++ src/server.js
@@ -8,5 +8,6 @@ const route = require('./router');
 const port = process.env.PORT || 5000;
 
 app.use('/movies', route);
+app.use('/', (req, res) => res.send("REST API MOVIES"));
 
 app.listen(port, () => console.log(`Start server on port ${port}`));
