762d3d93c32c434acbc95281148aa5392c3fe21e
igaryok <igor.sobko@gmail.com>
2019-11-20 16:15:06 +0200

    refactpred after reviwe

--- src/middleware.js
+++ src/middleware.js
@@ -6,8 +6,8 @@ exports.checkContentType = (req, res, next) => {
   const contentType = req.headers['content-type'];
 
   if (contentType !== 'application/json') {
-    handleError(res, 404, 'Content-Type is not b049a473-2b08-4624-9583-4c50511edc5e');
+    handleError(res, 400, 'Content-Type is not JSON');
+  } else {
+    next();
   }
-
-  next();
 }
--- src/models/movies-model.js
+++ src/models/movies-model.js
@@ -40,52 +40,41 @@ class MoviesModel {
     const query = {
       text: 'INSERT INTO movie(title, year, imdbrating) VALUES($1, $2, $3) RETURNING *;',
       values: [title, year, imdbRating],
-    }
+    };
 
     const res = await db.query(query);
+
     return res.rows[0];
   }
 
   async changeMovie(id, newDetails) {
-    const {title, year, imdbRating} = newDetails;
     const queryFind = {
       text: 'SELECT * FROM movie WHERE id = $1;',
       values: [id],
     };
     const foundMovie = await db.query(queryFind);
 
-    if (foundMovie) {
-      const chagedMovie = {
-        imdbRating: imdbRating || foundMovie.rows[0].imdbrating,
-        title: title || foundMovie.rows[0].title,
-        year: year || foundMovie.rows[0].year,
-      }
-
-      const queryUpdate = {
-        text: 'UPDATE movie SET imdbrating= $2, title = $3, year = $4 WHERE id = $1 RETURNING *;',
-        values: [id, chagedMovie.imdbRating, chagedMovie.title, chagedMovie.year],
+    const chagedMovie = {
+      ...foundMovie.rows[0],
+      ...newDetails
       };
+    
+    const queryUpdate = {
+      text: 'UPDATE movie SET imdbrating = $2, title = $3, year = $4 WHERE id = $1 RETURNING *;',
+      values: [id, chagedMovie.imdbrating, chagedMovie.title, chagedMovie.year],
+    };
 
-      const res = await db.query(queryUpdate);
-      return res.rows[0]
-    }
+    const res = await db.query(queryUpdate);
+    return res.rows[0]
   }
 
   async deleteMovie(id) {
-    const queryFind = {
-      text: 'SELECT * FROM movie WHERE id = $1;',
-      values: [id],
-    };
     const queryDelete = {
       text: 'DELETE FROM movie WHERE id = $1;',
       values: [id],
     };
-
-    const foundMovie = await db.query(queryFind);
     
-    if (foundMovie) {
-      await db.query(queryDelete);
-    }
+    await db.query(queryDelete);
   }
 }
 
